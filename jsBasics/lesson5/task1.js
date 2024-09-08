d2120234ada00c9813bfdaf127fd7e272d5ee1be
artem <artqae@gmail.com>
2024-09-07 22:17:26 -0700

    js conditions and loops

---
+++ jsBasics/lesson5/task1.js
@@ -0,0 +1,15 @@
+const averageGrade = 81;
+
+if (averageGrade < 60) {
+    console.log("Unsatisfactory");
+} else if (averageGrade >=60 && averageGrade <=70) {
+    console.log("Satisfactory");
+} else if (averageGrade >=71 && averageGrade <=80) {
+    console.log("Good");
+} else if (averageGrade >=81 && averageGrade <=90) {
+    console.log("Very good");
+} else if (averageGrade >=91 && averageGrade <=100) {
+    console.log("Perfectly");
+} else {
+    console.log("Invalid grade! Please check it and try again.");
+}
