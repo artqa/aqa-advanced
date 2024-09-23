91998f10a322de381e2e98b4a60d1d09abd058af
artem <artqae@gmail.com>
2024-09-22 21:46:46 -0700

    lesson9 hw objects

---
+++ jsBasics/lesson9/task1.js
@@ -0,0 +1,8 @@
+const student = {
+    firstName: "Tolya",
+    lastName: "Koval",
+    age: 30,
+    courses: ["Math", "History", "Programming"]
+};
+
+console.log(student);
---
+++ jsBasics/lesson9/task3.js
@@ -0,0 +1,16 @@
+
+const car1 = {
+    brand: "Wiesmann",
+    model: "GT MF5",
+    year: 2009
+};
+
+const car2 = {
+    brand: "ZAZ Tavria",
+    model: "ZAZ-11055",
+    owner: "Tolya"
+};
+
+const car3 = { ...car1, ...car2 };
+
+console.log(car3);
